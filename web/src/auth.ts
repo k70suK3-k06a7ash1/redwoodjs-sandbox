b5764be667902a5cc5def7d2a02c4010142cfb41
k70suK3-k06a7ash1 <2814109k@gmail.com>
2024-06-20 13:18:58 +0900

    update

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,5 +1,6 @@
 import { createDbAuthClient, createAuth } from '@redwoodjs/auth-dbauth-web'
+import WebAuthnClient from '@redwoodjs/auth-dbauth-web/webAuthn'
 
-const dbAuthClient = createDbAuthClient()
+const dbAuthClient = createDbAuthClient({ webAuthn: new WebAuthnClient() })
 
 export const { AuthProvider, useAuth } = createAuth(dbAuthClient)
